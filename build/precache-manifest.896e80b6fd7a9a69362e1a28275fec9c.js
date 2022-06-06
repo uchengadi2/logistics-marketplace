0a1b70e89eb32a55859be4c2493888a475c09a87
Uche Ngadi <uchengadi@yahoo.com>
2022-06-06 14:00:47 +0100

    added the build folder

---
+++ build/precache-manifest.896e80b6fd7a9a69362e1a28275fec9c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac89830390ce9ca389758d59259536c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0439c1bb2938e8a2f10b",
+    "url": "/static/css/main.8a8db68b.chunk.css"
+  },
+  {
+    "revision": "9897e56acb3b702ce65e",
+    "url": "/static/js/2.e709df71.chunk.js"
+  },
+  {
+    "revision": "0439c1bb2938e8a2f10b",
+    "url": "/static/js/main.90ceec48.chunk.js"
+  },
+  {
+    "revision": "56256c405bfa76082cb3",
+    "url": "/static/js/runtime~main.ea895c90.js"
+  },
+  {
+    "revision": "b32cfbf5718995263faf0de21be1193b",
+    "url": "/static/media/Car_Carrier_Trailer.b32cfbf5.png"
+  },
+  {
+    "revision": "7aaa4c698d65117391cfd66c18aa1a88",
+    "url": "/static/media/Cement_Truck.7aaa4c69.png"
+  },
+  {
+    "revision": "7136ff5a0318d191ccdf9e02965c328c",
+    "url": "/static/media/Chiller_Trucks.7136ff5a.png"
+  },
+  {
+    "revision": "350387f5ee9e1569dc00c4952fca7cc8",
+    "url": "/static/media/Custom Software Icon.350387f5.svg"
+  },
+  {
+    "revision": "577ee4aa8207aada876641948aa00177",
+    "url": "/static/media/Flat_bed_Trailer.577ee4aa.png"
+  },
+  {
+    "revision": "0c279c74329b60d2f9b4d20375197e14",
+    "url": "/static/media/Livestock_Trucks.0c279c74.png"
+  },
+  {
+    "revision": "3fa2954c189a0f3c85032e90824c8717",
+    "url": "/static/media/Logging_Trucks.3fa2954c.png"
+  },
+  {
+    "revision": "a326444ca796049aa49b2ebfcd840c7f",
+    "url": "/static/media/Tankers.a326444c.png"
+  },
+  {
+    "revision": "58258c57eb1f1efe729b6c9149103de3",
+    "url": "/static/media/Tipper_Trucks.58258c57.png"
+  },
+  {
+    "revision": "e9486d0a778b097697438ac2b26d9d26",
+    "url": "/static/media/Tow_Truck.e9486d0a.png"
+  },
+  {
+    "revision": "63803d1efe57ce16bdec2579a9c8a0d7",
+    "url": "/static/media/boat_haulage.63803d1e.png"
+  },
+  {
+    "revision": "3f832b6e5721eed869d4ac435e2dd9c3",
+    "url": "/static/media/box_truck.3f832b6e.png"
+  },
+  {
+    "revision": "6565588c8d5b3c6b2867d1d9037e4f21",
+    "url": "/static/media/cover_image2.6565588c.png"
+  },
+  {
+    "revision": "76baf4069a220dadc0eb2573710e20d5",
+    "url": "/static/media/cover_image_1.76baf406.png"
+  },
+  {
+    "revision": "9539f90d3389deb85c691ca1719f312a",
+    "url": "/static/media/crane truck2.9539f90d.png"
+  },
+  {
+    "revision": "e22079d988bd34dee9239a6288b42e7d",
+    "url": "/static/media/infoBackground.e22079d9.svg"
+  },
+  {
+    "revision": "9730290748805164acce0ccdd96d1529",
+    "url": "/static/media/logo.97302907.svg"
+  },
+  {
+    "revision": "eb953523f70c3832f9751c1081913138",
+    "url": "/static/media/mobileBackground.eb953523.jpg"
+  },
+  {
+    "revision": "6838a9fb85f01988c85e38475ae59fb0",
+    "url": "/static/media/mobileIcon.6838a9fb.svg"
+  },
+  {
+    "revision": "c608f23f6d6a6d579e2138bd77b6d054",
+    "url": "/static/media/repeatingBackground.c608f23f.svg"
+  },
+  {
+    "revision": "6b90d7dca8c7d0d5340cc0748c6bdb3c",
+    "url": "/static/media/websiteIcon.6b90d7dc.svg"
+  }
+]);
