e24f157be71c21b1609665daf7a4b83e44913ca3
Uche Ngadi <uchengadi@yahoo.com>
2022-06-24 11:52:47 +0100

    Editted the api Url for production

--- src/apis/local.js
+++ src/apis/local.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:5000/api/v1",//for the localhost
-  baseURL: "http://34.195.85.38/api/v1", // for production
+  baseURL: "https://api.brightwayafrica.com/api/v1", // for production
 });
