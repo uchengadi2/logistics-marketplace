214580859117f992557e30726bc9c2a350418b6c
Uche Ngadi <uchengadi@yahoo.com>
2022-06-24 10:29:35 +0100

    Modified the Url of the Api

--- src/apis/local.js
+++ src/apis/local.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  // baseURL: "http://localhost:5000/api/v1",//for the localhost
+  baseURL: "http://34.195.85.38/api/v1", // for production
 });
