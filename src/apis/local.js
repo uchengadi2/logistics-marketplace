9db0abf99fab11f69e2137885885825c600ed227
Uche Ngadi <uchengadi@yahoo.com>
2023-04-12 12:34:11 +0100

    change the api url

--- src/apis/local.js
+++ src/apis/local.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:5000/api/v1", //for the localhost
-  baseURL: "https://api.brightwayafrica.com/api/v1", // for production
+  baseURL: "https://api.udaraa.com/api/v1", // for production
 });
